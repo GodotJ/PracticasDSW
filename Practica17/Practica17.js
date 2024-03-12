9ad59bf20b34805fff1dbd9868be378cd41b3fab
GodotJ <junior.aguayo@uabc.edu.mx>
2024-03-11 19:31:00 -0700

    Practica 17

---
+++ Practica17/Practica17.js
@@ -0,0 +1,23 @@
+/*
+Realice las operaciones básicas: suma, resta, multiplicación y división de dos
+números.
+● Al final deberá presentar los resultados de todas las operaciones en un alert:
+● El resultado de la suma es: x, el de la resta es: y, el de la multiplicación es: z,
+y el de la división es: w.
+*Realice el ejercicio con datos ya predefinidos y también de forma en que el
+usuario ingrese los datos. */
+
+var num1 = 10;
+var num2 = 5;
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var div = num1 / num2;
+
+alert("La suma de los numeros es: " + suma);
+alert("La resta de los numeros es: " +resta);
+alert("La multiplicación de los numeros es: " + multi);
+alert("La división de los numeros es: "+div);
+
+
