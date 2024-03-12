0968d0cdbc12f4f7f39c4e8f3c6742970d8806ee
GodotJ <junior.aguayo@uabc.edu.mx>
2024-03-11 19:32:32 -0700

    Practica 18

---
+++ Practica18/Practica18.js
@@ -0,0 +1,16 @@
+/*
+Realice un programa que capture los días de la semana en un arreglo vacío.
+● Desplegar los días capturados en un alert.*/
+
+var arr = new Array();
+var arr = [];
+
+arr[0] = prompt("Ingrese el primer dia de la semana");
+arr[1] = prompt("Ingrese el segundo dia de la semana");
+arr[2] = prompt("Ingrese el tercer dia de la semana");
+arr[3] = prompt("Ingrese el cuarto dia de la semana");
+arr[4] = prompt("Ingrese el quinto dia de la semana");
+arr[5] = prompt("Ingrese el sexto dia de la semana");
+arr[6] = prompt("Ingrese el septimo dia de la semana");
+
+alert("Los dias de la semana son: " +arr)
